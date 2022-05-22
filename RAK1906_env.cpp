e899c2fa902975e047d4d0692318a92abb21d539
beegee-tokyo <bernd.giesecke@rakwireless.com>
2022-05-22 15:53:56 +0800

    Remove unused code

--- RAK1906_env.cpp
+++ RAK1906_env.cpp
@@ -9,65 +9,6 @@
  *
  */
 #include "main.h"
-// #include "rak1906.h"
-
-// /** BME680 instance for Wire */
-// rak1906 bme;
-
-// /**
-//  * @brief Initialize the BME680 sensor
-//  *
-//  * @return true if sensor was found
-//  * @return false if sensor was not found
-//  */
-// bool init_rak1906(void)
-// {
-// 	Wire.begin();
-
-// 	if (!bme.init())
-// 	{
-// 		MYLOG("BME", "Could not find a valid BME680 sensor, check wiring!");
-// 		return false;
-// 	}
-
-// 	// Set up oversampling and filter initialization
-// 	bme.setOversampling(TemperatureSensor, Oversample8);
-// 	bme.setOversampling(HumiditySensor, Oversample2);
-// 	bme.setOversampling(PressureSensor, Oversample4);
-// 	bme.setIIRFilter(IIR4);
-// 	bme.setGas(320, 150); // 320*C for 150 ms
-
-// 	return true;
-// }
-
-// /**
-//  * @brief Read environment data from BME680
-//  *     Data is added to Cayenne LPP payload as channels
-//  *     LPP_CHANNEL_HUMID_2, LPP_CHANNEL_TEMP_2,
-//  *     LPP_CHANNEL_PRESS_2 and LPP_CHANNEL_GAS_2
-//  *
-//  *
-//  * @return true if reading was successful
-//  * @return false if reading failed
-//  */
-// bool read_rak1906()
-// {
-// 	// MYLOG("BME", "Reading BME680");
-// 	if (bme.update())
-// 	{
-// 		g_solution_data.addRelativeHumidity(LPP_CHANNEL_HUMID_2, bme.humidity());
-// 		g_solution_data.addTemperature(LPP_CHANNEL_TEMP_2, bme.temperature());
-// 		g_solution_data.addBarometricPressure(LPP_CHANNEL_PRESS_2, bme.pressure());
-// 		g_solution_data.addAnalogInput(LPP_CHANNEL_GAS_2, bme.gas());
-// 	}
-// 	else
-// 	{
-// return false;
-// 	}
-// 	return true;
-// }
-
-/**********************************************************************************************/
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
 
